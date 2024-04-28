75528d04f30205abfe3732a32c57c78440fc2bda
ONE <one@SANDY.local>
2024-04-28 21:46:13 +1200

    add new class

---
+++ Husky.java
@@ -0,0 +1,6 @@
+class Husky extends Dog{
+
+    public void eye(){
+        System.out.println("I have blue eyes");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+    //Main method start here
+    public static void main(String[] args) {
+    Husky h1=new Husky();
+    h1.name="Blue";
+    h1.display();
+    h1.eye();
+    }
+}
